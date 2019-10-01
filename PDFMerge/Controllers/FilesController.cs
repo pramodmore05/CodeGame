cd567495c42da923ec2825b9e9b40c17555a640b
Nitin Varpe <Nitin.Varpe@wolterskluwer.com>
2019-10-01 19:12:32 +0530

    Added api controller and model.

---
+++ PDFMerge/Controllers/FilesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PDFMerge.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("[action]")]
+        public ActionResult<string> GetFiles()
+        {
+            var path = Environment.CurrentDirectory + "\files";
+            string[] files = System.IO.Directory.GetFiles(path, "*.pdf");
+
+            return Ok(path);
+        } 
+    }
+}
